4f07a1c9a9439436bdf3cc4980c443b704cdd876
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-13 23:34:52 -0500

    Config keys setup and connected to database

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
 
 const app = express();
 
 app.use(express.json());
+
+const db = config.get("mongoURI");
+
+//Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`MongoDB connected.`))
+  .catch(err => console.log(err));
 
 const PORT = 5000;
 
