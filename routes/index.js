86a970ac50a0fd8fadb34c1055f4752fc7c5b479
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-13 23:55:43 -0500

    Redirect url code route added

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
 
+const Url = require("../models/Url");
+
+// @route     GET /:code
+// @desc      Redirect to long/original URL
+router.get("/:code", (req, res) => {
+  Url.findOne({ urlCode: req.params.code })
+    .then(doc => {
+      return res.redirect(doc.longUrl);
+    })
+    .catch(err => {
+      return res
+        .status(400)
+        .json({ error: "Sorry this link may have expired" });
+    });
+});
+
 module.exports = router;
