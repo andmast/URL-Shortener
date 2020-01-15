5e66ccc0d6c64e492724459cc03c0a75e4646394
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-15 16:30:59 -0500

    Footer Created

---
+++ client/src/components/Layouts/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
+
+export default props => (
+  <AppBar position="static">
+    <Typography variant="headline" color="inherit">
+      Short.ly
+    </Typography>
+  </AppBar>
+);
