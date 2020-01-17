e3aecd1bd8add1076af40db68285d674d93e3385
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-16 21:49:34 -0500

    Import Url component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header, Footer } from "./Layouts";
+import Urls from "./Urls/Urls";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Urls />
       <Footer />
     </div>
   );
