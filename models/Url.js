d1a696d05bbbae2dd498c37f3dd923a5410259de
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-18 14:32:24 -0500

    Added visits counter to url model

--- models/Url.js
+++ models/Url.js
@@ -5,6 +5,7 @@ const urlSchema = new Schema({
   urlCode: String,
   longUrl: String,
   shortUrl: String,
+  visits: { type: Number, default: 0 },
   date: { type: String, default: Date.now }
 });
 
