ee76c6f32390cd983a4e5ec7364ee3722f1c9948
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-20 22:03:10 -0500

    Added Divder

--- client/src/components/Urls/Urls.js
+++ client/src/components/Urls/Urls.js
@@ -1,13 +1,20 @@
 import React from "react";
 import UrlsShortenForm from "./UrlShortenForm";
 import UrlList from "./UrlList";
+import { Segment, Divider, Header, Icon } from "semantic-ui-react";
 
 function Urls() {
   return (
-    <div className="Urls">
+    <Segment inverted>
       <UrlsShortenForm />
+      <Divider horizontal>
+        <Header as="h4" inverted>
+          <Icon name="list alternate" inverted />
+          Short.ly Urls Created
+        </Header>
+      </Divider>
       <UrlList />
-    </div>
+    </Segment>
   );
 }
 
