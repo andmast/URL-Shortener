55de1b4ecff87a2148cea4ab0940ff1257c0bac7
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-21 21:08:48 -0500

    Styling Header and footer

--- client/src/components/Layouts/Header.js
+++ client/src/components/Layouts/Header.js
@@ -1,31 +1,17 @@
 import React from "react";
-import { Menu, Container, Dropdown } from "semantic-ui-react";
+import { Menu, Container, Icon } from "semantic-ui-react";
 
-export default props => (
+export default () => (
   <div>
     <Menu fixed="top" inverted size="massive">
       <Container>
         <Menu.Item header>Short.ly</Menu.Item>
         <Menu.Item>Home</Menu.Item>
-
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
       </Container>
+
+      <Menu.Item>
+        <Icon name="github" size="big" />
+      </Menu.Item>
     </Menu>
   </div>
 );
