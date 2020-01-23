e8eb08fd118684c096c69bda6d26cbf591617b43
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-23 17:35:23 -0500

    Implement loading animation and added table header and table row components

--- client/src/components/Urls/UrlScoreboard.js
+++ client/src/components/Urls/UrlScoreboard.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { Table, Segment, Message } from "semantic-ui-react";
+import UrlTableRow from "./UrlTableRow";
+import UrlTableHeader from "./UrlTableHeader";
 
 function UrlScoreBoard(props) {
-  const { urls } = props;
-  return <div>{JSON.stringify(urls)}</div>;
+  const { urls, loading } = props;
+  return (
+    <Segment loading={loading}>
+      <Table celled>
+        <UrlTableHeader />
+        <Table.Body>
+          {urls ? (
+            urls.map(url => {
+              return <UrlTableRow url={url} key={url._id} />;
+            })
+          ) : (
+            <Table.Row>
+              <Table.Cell>
+                <Message header="Error!" content="Server Down" color="red" />
+              </Table.Cell>
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
+    </Segment>
+  );
 }
 
 export default UrlScoreBoard;
