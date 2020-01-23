166e703f381b444834a0fe8162f137bde1dd7ad7
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-23 15:14:30 -0500

    Removed container semantic ui

--- client/src/components/Urls/UrlList.js
+++ client/src/components/Urls/UrlList.js
@@ -1,51 +1,49 @@
 import React from "react";
-import { Container, Table, Segment, Message } from "semantic-ui-react";
+import { Table, Segment, Message } from "semantic-ui-react";
 
 function UrlList(props) {
   const { urlList } = props;
 
   return (
     <Segment>
-      <Container>
-        <Table celled>
-          <Table.Header>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Long Url</Table.HeaderCell>
+            <Table.HeaderCell>Short Url</Table.HeaderCell>
+            <Table.HeaderCell>Date Created</Table.HeaderCell>
+            <Table.HeaderCell>Number of visits</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {urlList.length > 0 ? (
+            urlList.map(url => {
+              return (
+                <Table.Row key={url._id}>
+                  <Table.Cell>{url.longUrl}</Table.Cell>
+                  <Table.Cell>
+                    <a
+                      href={url.shortUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {url.shortUrl}
+                    </a>
+                  </Table.Cell>
+                  <Table.Cell>{url.date}</Table.Cell>
+                  <Table.Cell>{url.visits}</Table.Cell>
+                </Table.Row>
+              );
+            })
+          ) : (
             <Table.Row>
-              <Table.HeaderCell>Long Url</Table.HeaderCell>
-              <Table.HeaderCell>Short Url</Table.HeaderCell>
-              <Table.HeaderCell>Date Created</Table.HeaderCell>
-              <Table.HeaderCell>Number of visits</Table.HeaderCell>
+              <Table.Cell>
+                <Message header="Error!" content="Server Down" color="red" />
+              </Table.Cell>
             </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {urlList.length > 0 ? (
-              urlList.map(url => {
-                return (
-                  <Table.Row key={url._id}>
-                    <Table.Cell>{url.longUrl}</Table.Cell>
-                    <Table.Cell>
-                      <a
-                        href={url.shortUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {url.shortUrl}
-                      </a>
-                    </Table.Cell>
-                    <Table.Cell>{url.date}</Table.Cell>
-                    <Table.Cell>{url.visits}</Table.Cell>
-                  </Table.Row>
-                );
-              })
-            ) : (
-              <Table.Row>
-                <Table.Cell>
-                  <Message header="Error!" content="Server Down" color="red" />
-                </Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
-      </Container>
+          )}
+        </Table.Body>
+      </Table>
     </Segment>
   );
 }
